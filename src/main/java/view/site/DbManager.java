42f495b675cc08ade179399e46f36772ffed3959
Smo17817 <simone.dassisi99@gmail.com>
2024-01-06 17:24:12 +0100

    [GSicilia]Aggiunti DbManager e MainContext in view.site

---
+++ src/main/java/view/site/DbManager.java
@@ -0,0 +1,72 @@
+package view.site;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+public class DbManager {
+	private List<Connection> freeDbConnections;
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("DB driver not found:" + e.getMessage());
+		}
+	}
+
+	public DbManager() {
+		freeDbConnections = new LinkedList<Connection>();
+	}
+
+	private static DbManager instance;
+
+	private synchronized Connection createDBConnection() throws SQLException {
+		Connection newConnection = null;
+		String ip = "localhost";
+		String port = "3306";
+		String db = "KawaiiComix";
+		String username = "root";
+		String password = "";
+
+		newConnection = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + db
+				+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+				username, password);
+		newConnection.setAutoCommit(false);
+		return newConnection;
+	}
+
+	public synchronized Connection getConnection() throws SQLException {
+		Connection connection;
+
+		if (!freeDbConnections.isEmpty()) {
+			connection = (Connection) freeDbConnections.get(0);
+			freeDbConnections.remove(0);
+
+			try {
+				if (connection.isClosed())
+					connection = getConnection();
+			} catch (SQLException e) {
+				connection.close();
+				connection = getConnection();
+			}
+		} else {
+			connection = createDBConnection();
+		}
+
+		return connection;
+	}
+
+	public static synchronized DbManager getInstance() {
+		if (instance == null) {
+			instance = new DbManager();
+		}
+		return instance;
+	}
+
+	public synchronized void releaseConnection(Connection connection) throws SQLException {
+		if (connection != null)
+			freeDbConnections.add(connection);
+	}
+}
---
+++ src/main/java/view/site/MainContext.java
@@ -0,0 +1,50 @@
+package view.site;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+
+@WebListener
+public class MainContext implements ServletContextListener {
+	
+
+	public void contextInitialized(ServletContextEvent sce) {
+		ServletContext context = sce.getServletContext();
+
+		DataSource ds = null;
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+			ds = (DataSource) envCtx.lookup("jdbc/kawaiicomix");
+
+		} catch (NamingException e) {
+			System.out.println("Error:" + e.getMessage());
+		}		
+		
+		context.setAttribute("DataSource", ds);
+		System.out.println("DataSource creation...."+ds.toString());
+		
+		
+		DbManager dm = null;
+		
+		dm = new DbManager();
+		context.setAttribute("DriverManager", dm);
+		System.out.println("DriverManager creation...."+dm.toString());		
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+		ServletContext context = sce.getServletContext();
+		
+		DataSource ds = (DataSource) context.getAttribute("DataSource");
+		System.out.println("DataSource deletion...."+ds.toString());
+		
+		DbManager dm = (DbManager) context.getAttribute("DriverManager");
+		System.out.println("DriverManager deletion...."+dm.toString());		
+	}
+}
