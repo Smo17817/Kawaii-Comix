87dc8c301d8f31a4151645612fee89642424376d
Smo17817 <simone.dassisi99@gmail.com>
2024-01-07 18:17:31 +0100

    Update classe prodotto

--- src/main/java/catalogoManagement/Prodotto.java
+++ src/main/java/catalogoManagement/Prodotto.java
@@ -20,6 +20,22 @@ public class Prodotto implements Serializable{
 	public Prodotto() {
 		super();
 	}
+	
+	// Ladifferenzasta nell'assenza di copieVendute
+	public Prodotto(String isbn, String nome, String autore, String descrizione, String immagine, Double prezzo,
+			Integer quantita, String genere, String categoria) {
+		super();
+		this.isbn = isbn;
+		this.nome = nome;
+		this.autore = autore;
+		this.descrizione = descrizione;
+		this.immagine = immagine;
+		this.prezzo = prezzo;
+		this.quantita = quantita;
+		this.genere = genere;
+		this.categoria = categoria;
+		this.copieVendute = 0;
+	}
 
 	public Prodotto(String isbn, String nome, String autore, String descrizione, String immagine, Double prezzo,
 			Integer quantita, String genere, String categoria, Integer copieVendute) {
@@ -34,6 +50,26 @@ public class Prodotto implements Serializable{
 		this.genere = genere;
 		this.categoria = categoria;
 		this.copieVendute = copieVendute;
+	}
+	
+	public void setNotEmpty(String nome, String autore, String descrizione, String immagine, Double prezzo,
+			Integer quantita, String genere, String categoria) {
+		if(!nome.equals(""))
+			this.nome = nome;
+		if(!autore.equals(""))
+			this.autore = autore;
+		if(!descrizione.equals(""))
+			this.descrizione = descrizione;
+		if(!immagine.equals(""))
+			this.immagine = immagine;
+		if(prezzo >= 0.0)
+			this.prezzo = prezzo;
+		if(quantita >= 0)
+			this.quantita = quantita;
+		if(!genere.equals(""))
+			this.genere = genere;
+		if(!categoria.equals(""))
+			this.categoria = categoria;	
 	}
 
 	public String getIsbn() {
