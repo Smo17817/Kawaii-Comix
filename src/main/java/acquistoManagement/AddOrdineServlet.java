e99c7ea7a73201603707226a23ed86afcacbacd5
Smo17817 <simone.dassisi99@gmail.com>
2024-01-08 15:33:16 +0100

    Aggiunta Servlet di creazione ordine a seguito di un acquisto

---
+++ src/main/java/acquistoManagement/AddOrdineServlet.java
@@ -0,0 +1,89 @@
+package acquistoManagement;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import catalogoManagement.Prodotto;
+import catalogoManagement.ProdottoIDS;
+import utenteManagement.User;
+
+@WebServlet("/AddOrdineServlet")
+public class AddOrdineServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private final DataSource ds = (DataSource) getServletContext().getAttribute("DataSource");
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		RequestDispatcher dispatcher = null;
+
+		User user = (User) session.getAttribute("user");
+		Carrello carrello = (Carrello) session.getAttribute("carrello");
+
+		try {
+			// Serve per ottenere la data al momento dell'ordine
+			Calendar c = Calendar.getInstance();
+			java.util.Date javaDate = c.getTime();
+			java.sql.Date sqlDate = new Date(javaDate.getTime());
+
+			double totale = Double.parseDouble(request.getParameter("totale"));
+			// Crea un ordineId randomico a 5 cifre
+			SecureRandom random = new SecureRandom();
+			int ordineId = 10000 + random.nextInt(90000);
+
+			ProdottoIDS prodottoIDS = new ProdottoIDS(ds);
+			OrdineIDS ordineIDS = new OrdineIDS(ds);
+			ordineIDS.doSaveOrdine(new Ordine(ordineId, sqlDate, totale, user.getId(), 1, 1));
+
+			String[] values = request.getParameter("quantita").split(",");
+			int i = 0;
+
+			// Aggiorna i prodotti nel db e aggiunge contestualmente gli ordini singoli
+			// associati a quello generale
+			for (Prodotto prodotto : carrello.getListaProdotti()) {
+				Integer quantitaScelta = Integer.parseInt(values[i]);
+				Double totParziale = prodotto.getPrezzo() * quantitaScelta;
+
+				// Modifica la quantità del prodotto in magazzino e le copie vendute in base
+				// all'acquisto attuale
+				prodotto.sommaQuantita(-quantitaScelta);
+				prodotto.sommaCopieVendute(+quantitaScelta);
+				prodottoIDS.doUpdateProdotto(prodotto);
+
+				OrdineSingolo ordineSingolo = new OrdineSingolo(quantitaScelta, totParziale, ordineId, prodotto);
+				ordineIDS.doSaveOrdineSingoloAssociato(ordineSingolo);
+
+				// Passa alla prossima quantità nella stringa con le quantità
+				i++;
+			}
+
+			carrello.empty();
+			session.setAttribute("carrello", carrello);
+
+			dispatcher = request.getRequestDispatcher("profilo.jsp");
+			dispatcher.forward(request, response);
+		} catch (SQLException | ServletException | IOException | NumberFormatException e) {
+			logger.log(Level.ALL, ERROR, e);
+		}
+	}
+
+	/*** LOGGER ***/
+	private static final Logger logger = Logger.getLogger(AddOrdineServlet.class.getName());
+	private static final String ERROR = "Errore";
+}
